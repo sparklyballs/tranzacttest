45a81b6832104170c9b8105da0df794f1fce6bcc
sparklyballs <sparklyballs@gmail.com>
2021-12-23 13:20:48 +0000

    add peer count variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -46,6 +46,14 @@ if [[ -n "${log_level}" ]]; then
   tranzact configure --log-level "${log_level}"
 fi
 
+if [[ -n "${peer_count}" ]]; then
+  tranzact configure --set-peer-count "${peer_count}"
+fi
+
+if [[ -n "${outbound_peer_count}" ]]; then
+  tranzact configure --set_outbound-peer-count "${outbound_peer_count}"
+fi
+
 sed -i 's/localhost/127.0.0.1/g' "$CONFIG_ROOT/config/config.yaml"
 
 exec "$@"
